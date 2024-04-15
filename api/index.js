d6b4b3e6c6467948807e236f2501b626270275cc
iamsara1 <142176070+iamsara1@users.noreply.github.com>
2024-04-16 01:01:54 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -22,11 +22,13 @@ mongoose
     console.log(err);
   });
 
-app.use(cors(cors: {
+app.use(cors(
+  {     
         origin: "*",
         methods: ["GET", "POST"],
-        credentials: true,
-    }));
+        credentials: true
+  }
+  ));
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
