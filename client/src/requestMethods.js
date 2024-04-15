3efbf74038d4621aa534b927c5ecf2b7a8fb2e8b
iamsara1 <142176070+iamsara1@users.noreply.github.com>
2024-04-16 01:53:33 +0200

    Update requestMethods.js

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://fitop-api.vercel.app/";
+const BASE_URL = "http://localhost:8080/api/";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
